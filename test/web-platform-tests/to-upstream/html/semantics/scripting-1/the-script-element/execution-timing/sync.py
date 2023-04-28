d3ce3374b50bf307864c94f421fc28b30e8d5646
Sourcery AI <>
2023-04-28 14:12:01 +0000

    'Refactored by Sourcery'

--- test/web-platform-tests/to-upstream/html/semantics/scripting-1/the-script-element/execution-timing/sync.py
+++ test/web-platform-tests/to-upstream/html/semantics/scripting-1/the-script-element/execution-timing/sync.py
@@ -7,6 +7,6 @@ def main(request, response):
     except:
         response.set_error(400, "Invalid parameter")
 
-    time.sleep(int(delay))
+    time.sleep(delay)
 
     return "window.newfunction = function () { log('run');};"
